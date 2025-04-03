f9f35d650d532e373a16946e6f10ac8369134eaf
Leona-hubert <leonakerlova@seznam.cz>
2025-04-03 20:26:23 +0200

    added modal

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const hamburger = document.getElementById("hamburger");
 const nav = queryselctor ("nav ul")
+;
+hamburger
+
+const modal = document.getElementById("modal")
+const modal = document.getElementById("openmodal")
+const modal = document.getElementById("zavrit")
+
+openmodalbtn.addEventlist(click (=>)
+MediaSourceHandle.classlist.add ("show");
+document.bodyclaslslist.add(modal-open";
+{
+    constructor(parameters) {
+        
+    }
+})
